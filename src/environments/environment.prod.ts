481216f5b99c6211bbd78138ff13a8db4ebfae65
Andrea Gonzales <AndreaGon@users.noreply.github.com>
2022-08-24 09:57:58 +0800

    Switch to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCGzT-5kzbim8B_9Do49Oy-qeEKfOJYCbM",
+    authDomain: "cdmcloud-c34d7.firebaseapp.com",
+    projectId: "cdmcloud-c34d7",
+    storageBucket: "cdmcloud-c34d7.appspot.com",
+    messagingSenderId: "184493253770",
+    appId: "1:184493253770:web:ed995aa758609bf6bfb973",
+    measurementId: "G-Z0P463D024"
+  }
 };
